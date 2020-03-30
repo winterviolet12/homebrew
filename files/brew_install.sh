775dc5dce415e53f12258a6f0eb640f53b1933da
winterviolet12 <winterviolet12@gmail.com>
2020-03-30 17:01:05 +0900

    add brew_install.sh

---
+++ files/brew_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# cask
+brew install cask
+
+# mas
+brew install mas
+
